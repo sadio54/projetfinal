225e8f4f237bb6d9421878159dcbba097bcedc14
sadio54 <137804138+sadio54@users.noreply.github.com>
2023-08-28 10:16:01 +0200

    Update StripeController.php

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -19,7 +19,7 @@ class StripeController extends AbstractController
     public function index(EntityManagerInterface $entityManager,  Cart  $cart, $reference): Response
     {
         $product_for_stripe = [];
-        $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+        $YOUR_DOMAIN = 'http://nationballer.mgk-formation.fr';
 
 
         $order = $entityManager->getRepository(Order::class)->findOneByReference($reference);
